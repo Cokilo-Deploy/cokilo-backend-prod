aad2ef0e2c4fc3de901c06235bd266b0e5917577
Cokilo <visiondevlop@gmail.com>
2025-09-08 18:18:45 +0100

    integration Stripe Connect 2 pour EU

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,6 +13,7 @@ interface UserAttributes {
   profileName?: string;
   phone: string;
   avatar?: string;
+  bio?: string;
   
   verificationStatus: UserVerificationStatus;
   stripeIdentitySessionId?: string;
@@ -63,6 +64,7 @@ class User extends Model<UserAttributes, UserCreationAttributes> implements User
   public lastName!: string;
   public phone!: string;
   public avatar?: string;
+  public bio?: string; 
   
   public verificationStatus!: UserVerificationStatus;
   public stripeIdentitySessionId?: string;
@@ -198,6 +200,10 @@ User.init({
     validate: {
       isUrl: true,
     },
+  },
+  bio: {
+    type: DataTypes.TEXT,
+    allowNull: true,
   },
   verificationStatus: {
     type: DataTypes.ENUM('unverified', 'pending', 'verified', 'failed', 'suspended'),
