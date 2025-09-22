7eb353f3de4b3049de0a00a81ee205fb601cc3ec
Cokilo <visiondevlop@gmail.com>
2025-09-22 23:01:29 +0100

    Formulaire de contact

--- src/routes/support.ts
+++ src/routes/support.ts
@@ -19,8 +19,8 @@ const emailTransporter = nodemailer.createTransport({
   port: parseInt(process.env.SMTP_PORT || '587'),
   secure: process.env.SMTP_SECURE === 'true',
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.SMTP_USER!,
+    pass: process.env.SMTP_PASS!,
   },
 });
 
