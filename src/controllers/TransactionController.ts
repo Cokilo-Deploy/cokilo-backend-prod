2ec2a6ba6b2178f8cd81fb949a4532aeefc8fe03
Cokilo <visiondevlop@gmail.com>
2025-09-28 15:33:59 +0100

    Supression de compte

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -18,7 +18,7 @@ import { sendLocalizedResponse } from '../utils/responseHelpers';
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-07-30.basil',
+  apiVersion: '2025-08-27.basil',
 });
 
 function generateRandomCode(): string {
--- src/services/StripeConnectService.ts
+++ src/services/StripeConnectService.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { User } from '../models/User';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-07-30.basil',
+  apiVersion: '2025-08-27.basil',
 });
 
 export class StripeConnectService {
