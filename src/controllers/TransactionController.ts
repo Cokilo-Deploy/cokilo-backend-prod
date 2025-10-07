c6e4337115c62395b350a578c7c30ae1eab7b084
Cokilo <visiondevlop@gmail.com>
2025-10-07 18:38:59 +0100

    Correction dafualt acces a la reservation pour les non verifiée

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -153,6 +153,13 @@ export class TransactionController {
       const user = (req as any).user;
       const { tripId, weight, description, itemType, specialInstructions } = req.body;
 
+      if (!user.isVerified) {
+      return res.status(403).json({
+        success: false,
+        message: 'Compte non vérifié'
+      });
+    }
+
       console.log('🔍 Données extraites:', { tripId, weight, description, itemType });
 
       if (!description || description.trim().length < 10) {
